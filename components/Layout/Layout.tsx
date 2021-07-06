1c88689a6aeb4c3a3e54683721da2c7590149431
Reubert Barbosa <reubertfernandes@gmail.com>
2021-07-05 23:10:13 -0300

    imp: Creating mobile first

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+export const siteTitle = "Reubert Barbosa";
+
+export const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Reubert Barbosa life portfolio's" />
+        <meta
+          property="og:url"
+          content="https://barbosa-reubert-blog.vercel.app/"
+        />
+        <meta name="og:title" content={siteTitle} />
+      </Head>
+      <main>{children}</main>
+    </div>
+  );
+};
